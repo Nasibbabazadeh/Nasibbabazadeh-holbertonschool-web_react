3b96e1fdc8935b917de0430f42b7baacc08480da
Nasib Babazade <143509503+Nasib-cmd@users.noreply.github.com>
2024-09-28 23:44:28 +0400

    last config

--- Typescript/task_4/js/main.ts
+++ Typescript/task_4/js/main.ts
@@ -3,9 +3,9 @@ const java = new Subjects.Java();
 const react = new Subjects.React();
 
 let cTeacher = {
-	firstName: 'Javad',
-	lastName: 'Javadov',
-	experienceTeachingC: 3,
+  firstName: 'Javad',
+  lastName: 'Javadov',
+  experienceTeachingC: 3,
 };
 
 cpp.setTeacher(cTeacher);
@@ -16,8 +16,8 @@ java.setTeacher(cTeacher);
 console.log(java.getRequirements());
 console.log(java.getAvailableTeacher());
 
-react.setTeacher(cTeacher) ;
+react.setTeacher(cTeacher);
 console.log(react.getRequirements());
 console.log(react.getAvailableTeacher());
 
-export {cpp,java,react,cTeacher}
+export { cpp, java, react, cTeacher };
