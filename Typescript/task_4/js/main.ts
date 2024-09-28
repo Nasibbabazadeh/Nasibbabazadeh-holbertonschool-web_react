008a35dcf0a2574e4ca73b348af9970b5d1fc3d5
Nasib Babazade <143509503+Nasib-cmd@users.noreply.github.com>
2024-09-28 23:33:42 +0400

    task 4 completed

---
+++ Typescript/task_4/js/main.ts
@@ -0,0 +1,23 @@
+const cpp = new Subjects.Cpp();
+const java = new Subjects.Java();
+const react = new Subjects.React();
+
+let cTeacher = {
+	firstName: 'Javad',
+	lastName: 'Javadov',
+	experienceTeachingC: 3,
+};
+
+cpp.setTeacher(cTeacher);
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher());
+
+java.setTeacher(cTeacher);
+console.log(java.getRequirements());
+console.log(java.getAvailableTeacher());
+
+react.setTeacher(cTeacher) ;
+console.log(react.getRequirements());
+console.log(react.getAvailableTeacher());
+
+export {cpp,java,react,cTeacher}
