830ee33bad46818ef5407047bbd53cbfda63cbb9
Nasib Babazade <143509503+Nasib-cmd@users.noreply.github.com>
2024-11-23 11:00:19 +0400

    enyzme setup

---
+++ react_intro/task_3/dashboard/src/setupTests.js
@@ -0,0 +1,4 @@
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16'; // Replace with your adapter if using a different version of React
+
+configure({ adapter: new Adapter() });
